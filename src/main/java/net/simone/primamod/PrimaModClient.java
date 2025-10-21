ea5cbf94647eea9cd2e6149568b4ecf16e23afbf
Simone Forloni <simoneforloni08@gmail.com>
2025-10-21 23:25:56 +0200

    setup

---
+++ src/main/java/net/simone/primamod/PrimaModClient.java
@@ -0,0 +1,11 @@
+package net.simone.primamod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PrimaModClient implements ClientModInitializer {
+	
+	@Override
+	public void onInitializeClient() {
+	
+	}
+}
