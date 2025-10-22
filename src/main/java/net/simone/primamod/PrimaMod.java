8dddf38dabf28e2483eec98005f280e7d860712f
Simone Forloni <simoneforloni08@gmail.com>
2025-10-22 14:49:45 +0200

    aggiungere primo item

--- src/main/java/net/simone/primamod/PrimaMod.java
+++ src/main/java/net/simone/primamod/PrimaMod.java
@@ -1,6 +1,7 @@
 package net.simone.primamod;
 
 import net.fabricmc.api.ModInitializer;
+import net.simone.primamod.item.ModItems;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,5 +12,6 @@ public class PrimaMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.initialize();
 	}
 }
--- src/main/java/net/simone/primamod/PrimaModClient.java
+++ src/main/java/net/simone/primamod/PrimaModClient.java
@@ -3,7 +3,6 @@ package net.simone.primamod;
 import net.fabricmc.api.ClientModInitializer;
 
 public class PrimaModClient implements ClientModInitializer {
-	
 	@Override
 	public void onInitializeClient() {
 	
