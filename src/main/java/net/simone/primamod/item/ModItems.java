047bef256769f75ba71d04bbdb4b65cbafe244b9
Simone Forloni <simoneforloni08@gmail.com>
2025-11-03 01:11:05 +0100

    fix launch crash

--- src/main/java/net/simone/primamod/item/ModItems.java
+++ src/main/java/net/simone/primamod/item/ModItems.java
@@ -5,6 +5,8 @@ import java.util.function.Function;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
@@ -27,7 +29,7 @@ public class ModItems {
 
 		Item item = itemFactory.apply(settings.registryKey(itemKey));
 
-	
+		Registry.register(Registries.ITEM, itemKey, item);
 
 		return item;
 	}
